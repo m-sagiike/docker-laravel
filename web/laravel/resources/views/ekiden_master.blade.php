b42657b20895e08a8756caf42eebf756a341c54d
sagiike <m.sagiike@outlook.jp>
2021-09-05 16:11:04 +0900

    駅伝マスタ画面を修正

--- web/laravel/resources/views/ekiden_master.blade.php
+++ web/laravel/resources/views/ekiden_master.blade.php
@@ -4,32 +4,33 @@
  
 @section('content')
 <p>駅伝マスタ</p>
+<div class="mb-3">
+  <button type="button" class="btn btn-primary">駅伝新規登録</button>
+</div>
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">駅伝名</th>
+      <th>大会記録</th>
+      <th scope="col"></th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">出雲全日本大学選抜駅伝競走（出雲駅伝）</th>
+      {{-- 大会記録マスタ画面へ遷移させる --}}
+      <td><button type="button" class="btn btn-primary">大会記録</button></td>
+      <td><button type="button" class="btn btn-success">編集</button></td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">全日本大学駅伝対校選手権大会</th>
+      <td><button type="button" class="btn btn-primary">大会記録</button></td>
+      <td><button type="button" class="btn btn-success">編集</button></td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">東京箱根間往復大学駅伝競走</th>
+      <td><button type="button" class="btn btn-primary">大会記録</button></td>
+      <td><button type="button" class="btn btn-success">編集</button></td>
     </tr>
   </tbody>
 </table>
